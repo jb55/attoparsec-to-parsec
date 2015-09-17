027169bf957826fa29e523d4122ebd9b5840ab06
William Casarin <bill@casarin.me>
2015-09-17 01:31:06 -0700

    Initial commit

---
+++ AttoToParsec.hs
@@ -0,0 +1,17 @@
+
+module AttoToParsec where
+
+import qualified Data.Attoparsec.ByteString as A
+import qualified Data.Attoparsec.Internal.Types as AI
+import qualified Text.Parsec as P
+import Data.ByteString
+import Data.Functor.Identity (Identity)
+
+attoToParsec :: A.Parser a -> P.Parsec ByteString () a
+attoToParsec atto = P.mkPT (convResult . stateResult atto)
+
+stateResult :: A.Parser a -> P.State ByteString () -> A.Result a
+stateResult atto st = A.parse atto (P.stateInput st)
+
+convResult :: A.Result r -> Identity (P.Consumed (Identity (P.Reply ByteString () r)))
+convResult (A.Fail bs ctxs msg) = undefined
