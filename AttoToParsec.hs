7abc3072cff2ded8ba39ace1fe7e945e3ed443c4
William Casarin <bill@casarin.me>
2015-09-17 01:32:50 -0700

    parseState

--- AttoToParsec.hs
+++ AttoToParsec.hs
@@ -7,10 +7,10 @@ import Data.ByteString
 import Data.Functor.Identity (Identity)
 
 attoToParsec :: A.Parser a -> P.Parsec ByteString () a
-attoToParsec atto = P.mkPT (convResult . stateResult atto)
+attoToParsec atto = P.mkPT (convResult . parseState atto)
 
-stateResult :: A.Parser a -> P.State ByteString () -> A.Result a
-stateResult atto st = A.parse atto (P.stateInput st)
+parseState :: A.Parser a -> P.State ByteString () -> A.Result a
+parseState atto st = A.parse atto (P.stateInput st)
 
 convResult :: A.Result r -> Identity (P.Consumed (Identity (P.Reply ByteString () r)))
 convResult (A.Fail bs ctxs msg) = undefined
